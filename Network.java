79f1590a3ab92317a113b34ddf7d8994b6c8f78b
adam <mada.carpm>
2025-01-07 01:03:42 +0200

    try5

--- Network.java
+++ Network.java
@@ -144,9 +144,9 @@ public class Network {
     // Returns a textual description of all the users in this network, and who they
     // follow.
     public String toString() {
-        String ans = "Network:\n";
+        String ans = "Network::";
         for (int i = 0; i < this.userCount; i++) {
-            ans += this.users[i].toString() + "\n";
+            ans +=  "\n" + this.users[i].toString();
         }
         return ans;
     }
